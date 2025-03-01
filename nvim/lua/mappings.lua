2ff79218a727e8258d865ef720765e409fa318d4
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2025-03-01 15:14:51 -0500

    add toggleterm and nvdash

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Toggle floating terminal
+map("n", "<leader>t", "<cmd>ToggleTerm direction=float<CR>", { desc = "Open floating terminal" })
+
+-- Toggle vertical split terminal
+map("n", "<leader>tv", "<cmd>ToggleTerm size=40 direction=vertical<CR>", { desc = "Open vertical terminal" })
+
+-- Toggle horizontal split terminal
+map("n", "<leader>th", "<cmd>ToggleTerm size=15 direction=horizontal<CR>", { desc = "Open horizontal terminal" })
